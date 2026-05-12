2e593a5f59abc2a447c73a88958c27e77e01f8fa
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-12 17:01:18 +0200

    feat: implement Soldier class and pass tests

--- src/Soldier.ts
+++ src/Soldier.ts
@@ -1,4 +1,17 @@
 export class Soldier {
-  constructor(healthArg, strengthArg) {}
+  health: number;
+  strength: number;
 
+  constructor(healthArg: number, strengthArg: number) {
+    this.health = healthArg;
+    this.strength = strengthArg;
+  }
+
+  attack(): number {
+    return this.strength;
+  }
+
+  receiveDamage(damage: number): void {
+    this.health -= damage;
+  }
 }
