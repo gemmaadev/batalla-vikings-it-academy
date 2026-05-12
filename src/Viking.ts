e11c7e37f9f938603401461de8b98fea662f1d4a
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-12 18:34:25 +0200

    feat: implement Viking class and pass tests

--- src/Viking.ts
+++ src/Viking.ts
@@ -1,1 +1,23 @@
-export class Viking{}
+import { Soldier } from "../src/Soldier";
+
+export class Viking extends Soldier {
+  name: string;
+
+  constructor(nameArg: string, healthArg: number, strengthArg: number) {
+    super(healthArg, strengthArg);
+    this.name = nameArg;
+  }
+
+  receiveDamage(damage: number): string {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry(): string {
+    return `Odin Owns You All!`;
+  }
+}
