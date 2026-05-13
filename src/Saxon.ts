88bc024b19235d1fd66ff8db38dd3c44167c7c53
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-13 13:30:17 +0200

    refactor(Saxon): simplify receiveDamage using early return

--- src/Saxon.ts
+++ src/Saxon.ts
@@ -9,8 +9,7 @@ export class Saxon extends Soldier {
     this.health -= damage;
     if (this.health > 0) {
       return `A Saxon has received ${damage} points of damage`;
-    } else {
-      return `A Saxon has died in combat`;
     }
+    return `A Saxon has died in combat`;
   }
 }
