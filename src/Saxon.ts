6c0e2b87c6f448e30d0bbcbbc121ebd0e1fb030a
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-13 11:16:41 +0200

    feat: implement Saxon class and pass tests

--- src/Saxon.ts
+++ src/Saxon.ts
@@ -1,1 +1,16 @@
-export class Saxon{}
+import { Soldier } from "./Soldier";
+
+export class Saxon extends Soldier {
+  constructor(healthArg: number, strengthArg: number) {
+    super(healthArg, strengthArg);
+  }
+
+  receiveDamage(damage: number): string {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
